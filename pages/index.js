b3fa43e57d84543b06d4e73f925b5aa211f478c3
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-10-27 22:11:07 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+const h1 = {
+  fontSize:'72pt',
+  fontWeight:'bold',
+  textAlign:'right',
+  letterSpacing:'-8px',
+  color:'f0f0f0',
+  margin:'-40px 0px'
+}
+
 export default () =><div>
   <h1>Next.js</h1>
   <div>Welcom to next.js!</div>

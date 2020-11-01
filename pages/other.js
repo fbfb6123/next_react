1fcc1b054f0fb8cd8924151c10fe6a43362e38ef
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-01 21:06:34 +0900

    Update other.js

--- pages/other.js
+++ pages/other.js
@@ -15,3 +15,13 @@ const p = {
   fontSize:'16pt'
 }
 
+export default () =><div>
+  <h1 style={h1}>Next.js</h1>
+  <p style={p}>Welcome to next.js!</p>
+  <hr />
+  <div>
+    <Link href="/other">
+      <a>Go to Other page &gt;&gt;</a>
+    </Link>
+  </div>
+</div>

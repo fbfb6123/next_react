2a5931d2c039a8857e7c03c0f77e9e0b507edac3
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-01 21:02:49 +0900

    Update other.js

--- pages/other.js
+++ pages/other.js
@@ -1,1 +1,17 @@
 import Link from 'next/link';
+
+const h1 = {
+  fontSize:'72pt',
+  fontWeight:'bold',
+  textAlign:'right',
+  letterSpacing:'-8px',
+  color:'#f0f0f0',
+  margin:'-40px 0px'
+}
+
+const p = {
+  margin:'0px',
+  color:'#666',
+  fontSize:'16pt'
+}
+

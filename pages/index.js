3b264e0453582d1b39d96677fd499975860c6251
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-04 04:04:42 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Link from 'next/link';
-import Layouts from '../components/Layout';
+import Layout from '../components/Layout';
 
 //外部スタイルシート呼び出し
 export default () =>(
   <Layout header="Next" title="Top page.">
-    <p>Welcome to next.js</p>
+    <p>Welcome to next.jsa</p>
     <hr/>
     <Link href="./other"><button>
       go to Other &gt;&gt;

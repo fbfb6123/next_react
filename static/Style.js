61fa28797536d79392521353a488da08d9a8c3ec
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-04 03:46:23 +0900

    Update Style.js

--- static/Style.js
+++ static/Style.js
@@ -26,16 +26,16 @@ export default <style>{`
     left: 10px;
   }
   h1 {
-    font-size:68pt;
+    font-size:22pt;
     font-weight:bold;
     text-align:right;
-    letter-spacing:-8px;
-    color:#f0f0f0;
-    margin:-32px 0px;
+    letter-spacing:0px;
+    color:#77a;
+    margin:-50px 0px 50px 0px;
   }
   p {
     margin:0px;
-    color:#666;
+    color:#669;
     font-size:16pt;
   }
 `}</style>;

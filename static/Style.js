f58c195239d49d074298613818ec19898fd2dcbd
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-04 03:42:35 +0900

    Update Style.js

--- static/Style.js
+++ static/Style.js
@@ -3,6 +3,16 @@ import css from 'styled-jsx/css';
 export default <style>{`
   body {
     margin:10px;
+    padding:5px;
+    color:#669;
+  }
+  header {
+    font-size:64pt;
+    font-weight:bold;
+    text-align:right;
+    letter-spacing:-8px;
+    color:#ddddff;
+    margin:-32px 5px;
   }
   h1 {
     font-size:68pt;

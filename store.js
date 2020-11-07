df87fecc95856cd4a42047f41e206a7e3d5ab3b2
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-07 23:42:01 +0900

    store.js

--- store.js
+++ store.js
@@ -12,25 +12,15 @@ const initial = {
 //レデューサー
 function counterReducer (state = initial, action) {
   switch (action.type) {
-    case 'INCREMENT':
-      return {
-        message: 'INCREMENT',
-        count: state.count +1
-      };
-    case 'DECREMENT':
-      return {
-        message: 'DECREMENT',
-        count:state.count -1
-      };
-    case 'RESET':
-      return {
-        message: 'RESET',
-        count: initial.count
-      };
-      default:
-        return state;
-  }
-}
+    //計算実行
+   let data2 = state.data.slice();
+   let s = action.value;
+   data2.unshift(s);
+   let num = s.replase(/[^0-9]/g,"");
+   let number2 = state.number.slice();
+   number2.unshift(num);
+   let result = (state.result * 1) + (num * 1);
+   
 
 //initStore関数(redux-store.jsで必要)
 export function initStore(state = initial) {

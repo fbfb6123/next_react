a4f7d6384de2021add3b7ed342b0f76432310645
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-07 23:12:35 +0900

    import追記

--- pages/other.js
+++ pages/other.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import Counter from '../components/Counter'
 
 export default () =>(
   <Layout header="Other" title="Other page.">

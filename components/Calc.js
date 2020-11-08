03f9552de06f39623c444683d700251c24d18961
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-08 23:49:42 +0900

    Update Calc.js

--- components/Calc.js
+++ components/Calc.js
@@ -1,3 +1,22 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
 
+class Calc extends Component {
+  style = {
+    fontSize:"12pt",
+    padding:"5px 10px"
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      input:'',
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
+    this.doAction = this.doAction.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  
+}

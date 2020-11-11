8600c0415feb775f7e9790c747283321ba379d25
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-11 23:45:23 +0900

    Update store.js

--- store.js
+++ store.js
@@ -2,6 +2,18 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import firebase from "firebase";
 
+// Firebaseの初期化
+var firebaseConfig = {
+  apiKey: "AIzaSyB3oCAV7BlVlWPrTD_wFf13jyoVt6sDyvY",
+  authDomain: "react-f5f3c.firebaseapp.com",
+  databaseURL: "https://react-f5f3c.firebaseio.com",
+  projectId: "react-f5f3c",
+  storageBucket: "react-f5f3c.appspot.com",
+  messagingSenderId: "851334510825",
+  appId: "1:851334510825:web:6e28f9153216d3be41d878",
+  measurementId: "G-9VSHFMFXGZ"
+};
+
 //ステート初期値
 const initial = {
   message:'START',

5154407e93b4a3f977e90bea0fbfb0d2408ed699
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-12 00:04:22 +0900

    Update fire.js

--- pages/fire.js
+++ pages/fire.js
@@ -2,3 +2,8 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import Sampledata from '../components/Sampledata';
 
+export default () =>(
+  <Layout header="Fire" title="Sample data.">
+    <Sampledata />
+  </Layout>
+);

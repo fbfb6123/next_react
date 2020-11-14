6a38419d98e7126ee696610e774083f3c6d2a272
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-14 23:17:49 +0900

    Update Firedind.js

--- components/Firedind.js
+++ components/Firedind.js
@@ -1,3 +1,12 @@
 import Rect, {Component} from 'rect'
 import firebase from "firebase";
 import "firebase/storage";
+
+class Firefind extends Component {
+  style = {
+    borderBottom:"1px solid gray"
+  }
+
+  //初期化。ステートとイベント用メソッドの設定
+  
+}

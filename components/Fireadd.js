9dda6960ad6ca06c306336c88285f001df5d4329
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-15 20:46:40 +0900

    constructorに初期値とアクションを定義

--- components/Fireadd.js
+++ components/Fireadd.js
@@ -2,3 +2,26 @@ import React, {Component} from 'react'
 import firebase from "firebase";
 import Router from 'next/router';
 import "firebase/storage";
+
+class Fireadd extends Component {
+  style = {
+    fontSize:"12pt",
+    padding:"5px 10px"
+  }
+
+  //初期化処理
+  constructor(props) {
+    super(props);
+    this.state = {
+      name_str:'',
+      msg_str:'',
+      lastID:-1,
+      data:[]
+    }
+    this.getLastID();//最後のIDチェック
+    this.doChangeName = this.doChangeName.bind(this);
+    this.doChangeMsg = this.doChangeMsg.bind(this);
+    this.doAction = this.doAction.bind(this);
+  }
+  
+}

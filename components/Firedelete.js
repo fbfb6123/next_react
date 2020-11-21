fe65e42f0796e260e0bdd6c5ebdbdbf774bab0d4
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-11-21 22:52:19 +0900

    FiredeleteClass生成
    import RouterはRouter,push（'/fire'）の記述のためのimport

--- components/Firedelete.js
+++ components/Firedelete.js
@@ -3,3 +3,11 @@ import firebase from "firebase";
 import Router from 'next/router';
 import "firebase/storage";
 
+class Firedelete extends Component {
+  style = {
+    fontsize:"12pt",
+    padding:"5px 10px"
+  }
+
+  
+}
